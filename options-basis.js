09aafaf92dd1e480cbf26e1454d0ca7aa7cb4bc5
Brennan Ives <brennanives@gmail.com>
2024-10-09 21:22:25 -0400

    added options-basis.js & improved readability of dividend-basis.js

--- options-basis.js
+++ options-basis.js
@@ -1,18 +1,18 @@
 // input values
 const shareCount = 542;
 const costBasis = 17.38;
-const totalDivReturn = 805;
+const totalOptionsReturn = 805;
 
 // functions 
-function getAdjustedBasis(shareCount, costBasis, totalDivReturn) {
+function getAdjustedBasis(shareCount, costBasis, totalOptionsReturn) {
   const totalInvested = (shareCount * costBasis);
-  const adjustedBasis = ((totalInvested - totalDivReturn) / shareCount);
+  const adjustedBasis = ((totalInvested - totalOptionsReturn) / shareCount);
  
   return adjustedBasis;
 }
 
 // call functions 
-const newBasis = getAdjustedBasis(shareCount, costBasis, totalDivReturn);
+const newBasis = getAdjustedBasis(shareCount, costBasis, totalOptionsReturn);
 const saved = ((costBasis - newBasis) / costBasis) * 100;
 
 // log values
@@ -20,4 +20,4 @@ console.log(
 `Current Basis: $${costBasis.toFixed(2)}
 Shares Owned: ${shareCount}
 Adjusted Basis: $${newBasis.toFixed(2)}
-Dividend Return = ${saved.toFixed(2)}%`)
+Return from Options: ${saved.toFixed(2)}%`)
